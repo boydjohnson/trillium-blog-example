663d5598ed7e159ec2910dc63faa1fc31d421952
Boyd Johnson <johnson.boyd@gmail.com>
2021-10-20 16:34:08 -0500

    Restrict use of UserAuth to json_web_token mod

--- api/src/json_web_token.rs
+++ api/src/json_web_token.rs
@@ -1,14 +1,19 @@
-use crate::users::UserAuth;
 use hmac::{Hmac, NewMac};
 use jwt::{SignWithKey, VerifyWithKey};
 use lazy_static::lazy_static;
 use sha2::Sha256;
 use std::{collections::BTreeMap, env::var};
-use trillium::Conn;
+use trillium::{Conn, Status};
 use types::users::User;
 
 lazy_static! {
     static ref KEY: String = var("SECRET_KEY").unwrap();
+}
+
+#[derive(Debug, Clone)]
+pub enum UserAuth {
+    User(User),
+    Forbidden,
 }
 
 pub fn create_key(user: &User) -> String {
@@ -46,9 +51,12 @@ pub async fn user_handler(conn: Conn) -> Conn {
     if let Some(auth) = conn.headers().get("Authorization").cloned() {
         if let Some(auth_bearer) = auth.as_str() {
             if let Some((_, jwt)) = auth_bearer.split_once(' ') {
-                conn.with_state(validate_signature(jwt.to_string()))
+                match validate_signature(jwt.to_string()) {
+                    UserAuth::User(u) => conn.with_state(u),
+                    UserAuth::Forbidden => conn.with_status(Status::Forbidden),
+                }
             } else {
-                conn.with_state(UserAuth::Forbidden)
+                conn.with_status(Status::Forbidden)
             }
         } else {
             conn.with_state(UserAuth::Forbidden)

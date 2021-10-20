50631c720cb2db86f9975b2c3c21e892f09aa32d
Boyd Johnson <johnson.boyd@gmail.com>
2021-10-19 19:58:12 -0500

    Add start to create_user db call

--- api/src/bin/blog.rs
+++ api/src/bin/blog.rs
@@ -1,11 +1,23 @@
+use dotenv::dotenv;
+use sqlx::{Pool, Postgres};
+use std::env::var;
+use trillium::State;
 use trillium_blog::router;
 use trillium_logger::Logger;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), sqlx::Error> {
+    dotenv().ok();
+
+    let postgres_url = var("DATABASE_URL").unwrap();
+
+    let pool = Pool::<Postgres>::connect(&postgres_url).await?;
+
     trillium_tokio::config()
         .with_port(8080)
         .with_host("127.0.0.1")
-        .run_async((Logger::new(), router()))
+        .run_async((Logger::new(), State::new(pool), router()))
         .await;
+
+    Ok(())
 }
